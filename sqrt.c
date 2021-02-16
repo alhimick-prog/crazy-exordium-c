8d381b68509c246ed877008a325b3423fd3dad8a
alhimick-prog <alhimick96@yahoo.com>
2021-02-16 22:05:50 +0300

    spaces

--- sqrt.c
+++ sqrt.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 #include <math.h>
 
-double discrim(double a, double b, double c)
+double discrim (double a, double b, double c)
 {
-    return b*b - 4*a*c;
+    return b * b - 4 * a * c;
 }
 int main()
 {
     double a, b, c, d;
     int n;
-    n = scanf("%lf %lf %lf", &a, &b, &c);
-    if(n != 3) {
-        printf("Error: wrong input.\n");
+    n = scanf ("%lf %lf %lf", &a, &b, &c);
+    if (n != 3) {
+        printf ("Error: wrong input.\n");
         return 2;
     }
-    if(a == 0) {
-        printf("It's not a quadratic equation!\n");
+    if (a == 0) {
+        printf ("It's not a quadratic equation!\n");
         return 2;
     }
-    d = discrim(a,b,c);
-    if(d < 0) {
-        printf("No roots\n");
+    d = discrim (a,b,c);
+    if (d < 0) {
+        printf ("No roots\n");
         return 0;
     }
-    d = sqrt(d);
-    printf("%.5f %.5f\n", (-b-d)/(2*a), (-b+d)/(2*a));
+    d = sqrt (d);
+    printf ("%.5f %.5f\n", (-b-d)/(2*a), (-b+d)/(2*a));
     return 0;
 }
