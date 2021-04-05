503895ec7d3a3d2237f58c1dee3314c2b545cc82
alhimick-prog <alhimick96@yahoo.com>
2021-04-05 22:37:40 +0300

    first C++

---
+++ C++/hello-fltk.cpp
@@ -0,0 +1,13 @@
+// fltk/hello-fltk.cpp
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+int main(int argc, char **argv)
+{
+    Fl_Window *win = new Fl_Window(300, 100, "Hello");
+    Fl_Box *b = new Fl_Box(0, 0, 300, 100, "Hello, world!");
+    b->labelsize(36);
+    win->end();
+    win->show();
+    return Fl::run();
+}
