e83e31881286ab1e8c812b9981c3a9bb345455bc
alhimick-prog <alhimick96@yahoo.com>
2021-04-23 15:37:06 +0300

    scelet23-04

--- C++/calculator.cpp
+++ C++/calculator.cpp
@@ -8,8 +8,14 @@
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Output.H>
 
-struct buttons {
+struct scelet {
+    Fl_Output *outp;
+    Fl_Input *inp;
+    Fl_Button *clear;
+    Fl_Button *del;
+    Fl_Button *extract_root;
     Fl_Button *numb[18];
+    Fl_Button *evaluation;
 };
 
 static const char *const buttsymb[] =
@@ -18,59 +24,53 @@ static const char *const buttsymb[] =
 
 enum {
     spacing = 3,
-    buttsize = 40,
-    doublebuttsize = 80,
-    field_w = 160,
+    butt_h = 40,
+    butt_w = 60,
+    double_butt_w = 120,
+    field_w = 240,
     quant_butt_in_str = 4
 };
 
 int main()
 {
-    int win_w = buttsize * 4 + spacing * 2;
-    int win_h = buttsize * 8 + spacing * 4;
+    int win_w = butt_w * 4 + spacing * 2;
+    int win_h = butt_h * 8 + spacing * 4;
     Fl_Window *win = new Fl_Window(win_w, win_h, "Calculator_scelet");
 
-    Fl_Output *res = new Fl_Output(spacing, spacing, field_w, buttsize);
-    Fl_Input *inp = new Fl_Input(spacing,
-                                 spacing * 2 + buttsize,
-                                 field_w,
-                                 buttsize);
+    scelet *gov = new scelet;
+    gov->outp = new Fl_Output(spacing, spacing, field_w, butt_h);
+    gov->inp = new Fl_Input(spacing, spacing * 2 + butt_h, field_w, butt_h);
 
-    int butt_y = spacing * 3 + buttsize * 2;
-    Fl_Button *clear = new Fl_Button(spacing,
-                                     butt_y,
-                                     doublebuttsize,
-                                     buttsize,
-                                     "C");
-    Fl_Button *del = new Fl_Button(spacing + doublebuttsize,
-                                   butt_y,
-                                   buttsize,
-                                   buttsize,
-                                   "<-");
-    Fl_Button *extract_root = new Fl_Button(spacing + buttsize * 3,
-                                        butt_y,
-                                        buttsize,
-                                        buttsize,
-                                        "Root");
+    int butt_y = spacing * 3 + butt_h * 2;
+    gov->clear = new Fl_Button(spacing, butt_y, double_butt_w, butt_h, "C");
+    gov->del = new Fl_Button(spacing + double_butt_w,
+                             butt_y,
+                             butt_w,
+                             butt_h,
+                             "<-");
+    gov->extract_root = new Fl_Button(spacing + butt_w * 3,
+                                      butt_y,
+                                      butt_w,
+                                      butt_h,
+                                      "Root");
     
-    buttons *butt = new buttons;
     for (int i = 0; i < 18; i++) {
         int butt_x = spacing;
-        int butt_y = spacing * 3 + buttsize * 3;
+        int butt_y = spacing * 3 + butt_h * 3;
         int x_pos = i % 4;
         int y_pos = i / 4;
-        butt->numb[i] = new Fl_Button(butt_x + x_pos * buttsize,
-                                         butt_y + y_pos * buttsize,
-                                         buttsize,
-                                         buttsize,
-                                         buttsymb[i]);
+        gov->numb[i] = new Fl_Button(butt_x + x_pos * butt_w,
+                                     butt_y + y_pos * butt_h,
+                                     butt_w,
+                                     butt_h,
+                                     buttsymb[i]);
     };
 
-    Fl_Button *evaluation = new Fl_Button(spacing + buttsize * 2,
-                                          spacing * 3 + buttsize * 7,
-                                          doublebuttsize,
-                                          buttsize,
-                                          "=");
+    gov->evaluation = new Fl_Button(spacing + butt_w * 2,
+                                    spacing * 3 + butt_h * 7,
+                                    double_butt_w,
+                                    butt_h,
+                                    "=");
 
     win->end();
     win->show();
